7742ce519284b02c82cbe445906a9500e662adb2
Robert Maneno <robertjmaneno@gmail.com>
2025-06-20 14:42:53 +0200

    Add .gitignore for Python project files, update alembic.ini for script location, and modify user model fields. Refactor migrations environment setup for better integration with SQLModel.

--- app/models/user.py
+++ app/models/user.py
@@ -5,8 +5,8 @@ class User(SQLModel, table=True):
     id : Optional[int] = Field(default=True,  primary_key=True)
     first_name: str = Field(max_length=50, index=True)
     last_name: str = Field(max_length=50, index=True)
-    email: str = Field(email= True, unique=True, index=True)
-    password: str = Field(password=True)
-    confirm_password: str = Field(password = True) 
+    email: str = Field(unique=True, index=True)
+    password: str 
+    confirm_password: str 
 
 
--- migrations/env.py
+++ migrations/env.py
@@ -1,37 +1,30 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-from app.core.config import settings
-
+from sqlalchemy import create_engine
 from alembic import context
+from sqlmodel import SQLModel
+from app.core.config import settings
+from app.models.user import User # Import all models here
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Alembic Config object
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Set up logging
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# Model metadata for autogenerate
+target_metadata = SQLModel.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
+# Create engine using DATABASE_URL from settings
+engine = create_engine(settings.DATABASE_URL)
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode."""
-    url = settings.DATABASE_URL
     context.configure(
-        url=url,
+        url=settings.DATABASE_URL,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -42,12 +35,7 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode."""
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool)
-
-    with connectable.connect() as connection:
+    with engine.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=target_metadata
